ecf7f5e342eb80cd1207a10bbf158b0f8abfaffa
Bank <sarawut.ntw@gmail.com>
2026-02-03 22:10:58 +0700

    updata

--- calculator.c
+++ calculator.c
@@ -1,1 +1,50 @@
 #include <stdio.h>
+
+int add(int a, int b);
+
+int sub(int a, int b);
+
+int main() {
+
+    int num1, num2, result;
+
+    char op;
+
+    printf("enter first num: ");
+
+    scanf("%d", &num1);
+
+    printf("enter operator (+ or -): ");
+
+    scanf(" %c", &op);   
+
+    
+    printf("enter second num: ");
+
+    scanf("%d", &num2);
+
+
+    result = add(num1, num2);
+
+
+    printf("\nprocess:\n");
+
+    printf("%d %c %d\n", num1, op, num2);
+
+    printf("result:\n");
+
+    printf("%d\n", result);
+
+    return 0;
+}
+
+int add(int a, int b) {
+
+    return a + b;
+}
+
+
+int sub(int a, int b) {
+    
+    return a - b;
+}
